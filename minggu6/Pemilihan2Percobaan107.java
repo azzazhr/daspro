7ca5a3f864e82158e1c4fcfedaa1d2a94b27f955
azzazhr <azzahraqina70@gmail.com>
2023-10-03 14:54:50 +0700

    modifikasi

--- minggu6/Pemilihan2Percobaan107.java
+++ minggu6/Pemilihan2Percobaan107.java
@@ -9,12 +9,9 @@ public class Pemilihan2Percobaan107 {
         if ((tahun % 4) == 0) {
             if (tahun % 100 != 0)
                 System.out.println("Tahun Kabisat");
-            else if (tahun % 100 == 0)
-                if (tahun % 400 == 0)
-                    System.out.println("Tahun Kabisat");
-            else
-               System.out.println("Bukan Tahun Kabisat");
-        } 
-    }
+        } else
+              System.out.println("Bukan Tahun Kabisat");
+
+           }
 }
 
