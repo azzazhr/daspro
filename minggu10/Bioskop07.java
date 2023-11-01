0540731224b364a6f459a26dd160e9b1412825d0
azzazhr <azzahraqina70@gmail.com>
2023-11-01 21:31:03 +0700

    menambahkan program percobaan1 no 10

--- minggu10/Bioskop07.java
+++ minggu10/Bioskop07.java
@@ -26,7 +26,7 @@ public class Bioskop07 {
         System.out.println("Penonton pada baris ke-3: ");
         for (int i = 0; i < penonton[2].length; i++) {
             System.out.println(penonton[2][i]);
-
         }
+
     }
 }
