25f6dc9044e3be317f188344852152e93ca0e04c
azzazhr <azzahraqina70@gmail.com>
2023-11-06 19:52:17 +0700

    add percobaan nestedloop

---
+++ minggu11/NestedLoop_2341720224.java
@@ -0,0 +1,28 @@
+package minggu11;
+
+import java.util.Scanner;
+
+public class NestedLoop_2341720224 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[][] temps = new double[5][7];
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("Kota ke-" + i);
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print("Hari ke-" + (j + 1) + ": ");
+                temps[i][j] = sc.nextDouble();
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("Kota ke-" + ": ");
+            for (int j = 0; j < temps.length; j++) {
+                System.out.println(temps[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
