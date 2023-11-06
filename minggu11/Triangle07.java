f6dd1a15097868de41fdce27667bff4599c662d7
azzazhr <azzahraqina70@gmail.com>
2023-11-06 19:51:53 +0700

    add percobaan triangle

---
+++ minggu11/Triangle07.java
@@ -0,0 +1,21 @@
+package minggu11;
+
+import java.util.Scanner;
+
+public class Triangle07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N = ");
+        int N = sc.nextInt();
+        int i = 0;
+        while (i <= N) {
+            int j = 0;
+            while (j < i) {
+                System.out.print("*");
+                j++;
+            }
+            i++;
+        }
+    }
+}
