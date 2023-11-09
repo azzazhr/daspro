633bb1f1622ef034b53175aaaa1e75b687913cb8
azzazhr <azzahraqina70@gmail.com>
2023-11-09 19:34:34 +0700

    add percobaan2 no1

--- minggu11/Square07.java
+++ minggu11/Square07.java
@@ -9,11 +9,10 @@ public class Square07 {
         System.out.print("Masukkan nilai N =");
         int N = sc.nextInt();
 
-        for (int iOuter = 1; iOuter <= N; iOuter++) {
+        for (int iOuter = 0; iOuter <= N; iOuter++) {
             for (int i = 1; i <= N; i++) {
                 System.out.print("*");
             }
-            System.out.println();
         }
 
     }
