fa929a2cf7942550a4dd60a7ecbba5e7abe23e73
azzazhr <azzahraqina70@gmail.com>
2023-11-20 15:08:15 +0700

    add percobaan terimakasih

---
+++ minggu13/TerimaKasih_07.java
@@ -0,0 +1,12 @@
+package minggu13;
+
+public class TerimaKasih_07 {
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you for being the best teacher in the world. \n"
+                + "You inspired in me a love learning and made me feel like I could ask you anything.");
+    }
+
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
+}
