83c69e9272f8c413e6beaaa8b133ceb5c2144fcd
azzazhr <azzahraqina70@gmail.com>
2023-11-20 18:21:23 +0700

    add percobaan (Ucapan)

--- minggu13/Ucapan_07.java
+++ minggu13/Ucapan_07.java
@@ -13,6 +13,6 @@ public class Ucapan_07 {
 
     public static void main(String[] args) {
         String nama = PenerimaUcapan();
-        System.out.println("Thank you" + nama + "\nMay the force be with you.");
+        System.out.println("Thank you " + nama + "\nMay the force be with you.");
     }
 }
