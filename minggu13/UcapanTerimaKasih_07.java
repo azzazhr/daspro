ffe2aa4b0a8decbc70e86cb7f6f6d5d3531cdf2e
azzazhr <azzahraqina70@gmail.com>
2023-11-25 11:26:55 +0700

    modify percobaan 4 no 3

--- minggu13/UcapanTerimaKasih_07.java
+++ minggu13/UcapanTerimaKasih_07.java
@@ -13,7 +13,7 @@ public class UcapanTerimaKasih_07 {
 
     public static void UcapanTerimaKasih() {
         String nama = PenerimaUcapan();
-        System.out.println("Thank you" + nama + " for being the best teacher in the world.\n"
+        System.out.println("Thank you " + nama + " for being the best teacher in the world.\n"
                 + "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
 
@@ -23,7 +23,7 @@ public class UcapanTerimaKasih_07 {
 
     public static void main(String[] args) {
         UcapanTerimaKasih();
-        String ucapan = ".";
+        String ucapan = "Thank you...";
         ucapanTambahan(ucapan);
 
     }
