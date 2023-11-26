a6e36e7d8d97129d7adffd0baafb6157fdd72489
azzazhr <azzahraqina70@gmail.com>
2023-11-26 12:38:21 +0700

    modify percobaan1 (tanpa fungsi)

--- minggu13/TerimaKasih_07.java
+++ minggu13/TerimaKasih_07.java
@@ -1,19 +1,16 @@
 package minggu13;
 
 public class TerimaKasih_07 {
-    public static void ucapanTerimaKasih() {
-        System.out.println("Thank You for being the best teacher in the world.\n" +
-                "You inspired in me a love for learning and made me feel like I could ask you anything.");
-    }
-
-    public static void ucapanTambahan(String ucapan) {
-        System.out.println(ucapan);
-    }
 
     public static void main(String[] args) {
-        ucapanTerimaKasih();
-        String ucapan = "Terimakasih Pak.. Bu.. semoga sehat selalu";
-        ucapanTambahan(ucapan);
+        System.out.println("Thank You for being the best teacher in the world.\n" +
+                "You inspired in me a love for learning and made me feel like I could ask you anything.");
 
+        // UcapanTerimakasih();
+
+        String ucapan = "Terimakasih Pak... Bu... Semoga sehat selalu";
+        // UcapanTambahan(ucapan
+        ;
     }
+
 }
