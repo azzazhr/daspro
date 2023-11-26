89207d00fd80a80eac175584358f601b0271b109
azzazhr <azzahraqina70@gmail.com>
2023-11-26 12:42:24 +0700

    add programku (percobaan6 no 2)

--- minggu13/Programku.java
+++ minggu13/Programku.java
@@ -1,14 +1,14 @@
 package minggu13;
 
 public class Programku {
-     public static void tampilHinggaKei(int i) {
-        for(int j = 1;j<=i;j++){
+    public static void tampilHinggaKei(int i) {
+        for (int j = 1; j <= i; j++) {
             System.out.print(j);
         }
     }
-    
-    public static int Jumlah (int bil1, int bil2){
-        return(bil1 + bil2);
+
+    public static int Jumlah(int bil1, int bil2) {
+        return (bil1 + bil2);
     }
 
     public static void TampilJumlah(int bil1, int bil2) {
@@ -16,7 +16,8 @@ public class Programku {
     }
 
     public static void main(String[] args) {
-        int temp = Jumlah(1,1);
+        int temp = Jumlah(1, 1);
         TampilJumlah(temp, 5);
+        
     }
 }
